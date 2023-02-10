e52bd016d2cd741b01b3442b4b1f7679e119b78d
99131Anthony <113529462+99131Anthony@users.noreply.github.com>
2023-02-10 10:20:11 -0500

    Add files via upload

--- pygame/click_game.py
+++ pygame/click_game.py
@@ -1,3 +1,5 @@
+import time
+
 ball = Actor('character_n')
 ball.pos = 100, 50
 
@@ -8,8 +10,23 @@ def draw():
     screen.fill((51,17,34))
     ball.draw()
 
+def set_ball_sad():
+    ball.image:'character_H'
+
 def update():
-    ball.left = ball.left + 501
+    ball.left = ball.left + 6
     if ball.left > WIDTH:
         ball.right = 0
+
+def on_mouse_down(pos):
+    if ball.collidepoint(pos):
+        sounds.grunt.play()
+        set_ball_sad()
+        ball.image='character_h'
+        time.sleep(1)
+
+        print("You monster!")
+    else:
+        sounds.boing.play()
+        print("BUt luCk NExT tIMe STo0biD!")
 
